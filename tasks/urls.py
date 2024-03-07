24e7c375361320685fc226949769e3dc44fc3c73
EDDY <eddmunyiri@gmail.com>
2024-03-07 15:26:48 +0300

    templates and class views config

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.startingpage, name="index")
+    path("", views.StartingPage.as_view(), name="tasks"),
+    path('task/<int:pk>/', views.TaskDetail.as_view(), name='task_detail'),
+    path('create_task/', views.CreateTask.as_view(), name="task_create")
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
-
+from . models import Task
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 # Create your views here.
 
 
-def startingpage(request):
-    return render(request, "tasks/index.html")
+class StartingPage(ListView):
+    model = Task
+    template_name = 'tasks/index.html'
+    context_object_name = 'tasks'
+
+
+class TaskDetail(DetailView):
+    model = Task
+    template_name = 'tasks/task_detail.html'
+    context_object_name = "task"
+
+
+class CreateTask(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
